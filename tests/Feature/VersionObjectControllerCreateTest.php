27fb18c8a455cf8e8bb1d8e9ddacbb5cafd9fce4
Eddie <eamoto@live.com>
2024-06-24 00:32:37 +0800

    debug feature testing in github

--- tests/Feature/VersionObjectControllerCreateTest.php
+++ tests/Feature/VersionObjectControllerCreateTest.php
@@ -6,11 +6,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class VersionObjectControllerCreateTest extends VersionObjectControllerBaseTest
+class VersionObjectControllerCreateTest extends TestCase
 {
+    use RefreshDatabase;
+    
     public function test_create_version_object(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => "value1"]);
+        $res = $this->postJson('/api/object/', ["mykey" => "value1"]);
 
         $res->assertStatus(200)
             ->assertJsonPath("data.key", "mykey")
@@ -19,49 +21,49 @@ class VersionObjectControllerCreateTest extends VersionObjectControllerBaseTest
 
     public function test_create_version_object_with_multiple_key_value_pair(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => "value1", "mykey2" => "value1"]);
+        $res = $this->postJson('/api/object/', ["mykey" => "value1", "mykey2" => "value1"]);
         $res->assertUnprocessable();
     }
 
     public function test_create_version_object_with_null_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => null]);
+        $res = $this->postJson('/api/object/', ["mykey" => null]);
         $res->assertUnprocessable();
     }
 
     public function test_create_version_object_with_empty_string_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => ""]);
+        $res = $this->postJson('/api/object/', ["mykey" => ""]);
         $res->assertUnprocessable();
     }
 
     public function test_create_version_object_with_spaces_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => "              "]);
+        $res = $this->postJson('/api/object/', ["mykey" => "              "]);
         $res->assertUnprocessable();
     }
 
     public function test_create_version_object_with_number_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => 111]);
+        $res = $this->postJson('/api/object/', ["mykey" => 111]);
         $res->assertUnprocessable();
     }
 
     public function test_create_version_object_with_empty_array_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => []]);
+        $res = $this->postJson('/api/object/', ["mykey" => []]);
         $res->assertStatus(200);
     }
 
     public function test_create_version_object_with_non_empty_array_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => [1, 2, 3, 4]]);
+        $res = $this->postJson('/api/object/', ["mykey" => [1, 2, 3, 4]]);
         $res->assertStatus(200);
     }
 
     public function test_create_version_object_with_non_empty_assoc_array_value(): void
     {
-        $res = $this->postJson(self::REQ_URI, ["mykey" => ["key1" => "1", "key2" => "2"]]);
+        $res = $this->postJson('/api/object/', ["mykey" => ["key1" => "1", "key2" => "2"]]);
         $res->assertStatus(200);
     }
 }
