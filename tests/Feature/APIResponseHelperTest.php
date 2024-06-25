b4f6f9186087fd503e5cac73e1c689a301b00413
Eddie <eamoto@live.com>
2024-06-25 12:03:11 +0800

    test api data structure

---
+++ tests/Feature/APIResponseHelperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class APIResponseHelperTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_api_response_structure_success(): void
+    {
+        $res = $this->postJson('/api/object/', ["mykey" => "value1"]);
+        $res->assertStatus(200)
+            ->assertJsonStructure(["code", "messages", "data"]);
+    }
+
+    public function test_api_response_structure_fail(): void
+    {
+        $res = $this->postJson('/api/object/', ["mykey" => null]);
+        $res->assertUnprocessable()
+            ->assertJsonStructure(["code", "messages"]);
+    }
+
+}
